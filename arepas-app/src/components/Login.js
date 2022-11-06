ccbe242b4a1d42d15b1bcdb400b8961befb7c108
Yulian <julianolomanolox@hotmail.com>
2022-11-06 01:17:33 -0500

    pequeños cambios

--- arepas-app/src/components/Login.js
+++ arepas-app/src/components/Login.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { Container } from 'react-bootstrap';
 
 export const Login = () => (
+  <Container className="d-flex justify-content-start mt-3" >
   <Form>
     {/* email */}
       <Form.Group className="mb-3" controlId="formBasicEmail">
+        <div className='d-flex justify-content-start'>
         <Form.Label>Email address</Form.Label>
+        </div>
         <Form.Control type="email" placeholder="Enter email" />
       </Form.Group>
 
       {/* contraseña */}
       <Form.Group className="mb-3" controlId="formBasicPassword">
+      <div className='d-flex justify-content-start'>
         <Form.Label>Password</Form.Label>
+        </div>
         <Form.Control type="password" placeholder="Password" />
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
       </Button>
     </Form>
+    </Container>
 )
--- arepas-app/src/components/Register.js
+++ arepas-app/src/components/Register.js
@@ -1,36 +1,48 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { Container } from 'react-bootstrap';
 
 export const Register = () => (
+  <Container className="d-flex justify-content-start mt-3">
   <Form>
     {/* email */}
       <Form.Group className="mb-3" controlId="formBasicEmail">
+      <div className='d-flex justify-content-start'>
         <Form.Label>Email</Form.Label>
+      </div>
         <Form.Control type="email" placeholder="ingrese su email" />
       </Form.Group>
 
       {/* contraseña */}
       <Form.Group className="mb-3" controlId="formBasicPassword">
+      <div className='d-flex justify-content-start'>
         <Form.Label>contraseña</Form.Label>
+      </div>
         <Form.Control type="password" placeholder="ingrese su contraseña" />
       </Form.Group>
 
       {/* primer nombre */}
       <Form.Group className="mb-3" controlId="formBasicPassword">
+      <div className='d-flex justify-content-start'>
         <Form.Label>primer nombre</Form.Label>
+        </div>
         <Form.Control type="text" placeholder="ingrese su primer nombre" />
       </Form.Group>
 
       {/* segundo nombre */}
       <Form.Group className="mb-3" controlId="formBasicPassword">
+      <div className='d-flex justify-content-start'>
         <Form.Label>segundo nombre</Form.Label>
+        </div>
         <Form.Control type="text" placeholder="ingrese su segundo nombre" />
       </Form.Group>
       
       {/* direcion */}
       <Form.Group className="mb-3" controlId="formBasicAdress">
+      <div className='d-flex justify-content-start'>
         <Form.Label>direcion</Form.Label>
+        </div>
         <Form.Control type="text" placeholder="ingrese su direcion" />
       </Form.Group>
 
@@ -39,4 +51,5 @@ export const Register = () => (
         Submit
       </Button>
     </Form>
+    </Container>
 )
