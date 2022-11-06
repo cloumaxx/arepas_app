c948c127fc2d120f13907ae72466e442e9eab959
marcook888 <marcosrojas772@gmail.com>
2022-11-06 17:33:05 -0500

    register ajua

--- arepas-app/src/index.js
+++ arepas-app/src/index.js
@@ -1,27 +1,18 @@
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { createSlice, configureStore} from '@reduxjs/toolkit'
-import allReducers from './reducers/reducers'
-import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
+import App from './routes/App';
+import bootstrap from "bootstrap";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap';
 
-export const store = configureStore({
-  reducer: allReducers
-})
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-  <Provider store={store}>
-  <App />
-  </Provider>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
