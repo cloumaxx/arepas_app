f909e7e32b64864c8f19476d70e0a52d101b3c9a
Yulian <julianolomanolox@hotmail.com>
2022-11-07 00:37:37 -0500

    falta mejorar todas las vistas y añadir registro

--- arepas-app/src/components/Login.js
+++ arepas-app/src/components/Login.js
@@ -1,41 +1,90 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { Col, Container, Row } from 'react-bootstrap';
 import logo from '../imagenes/arepagif.gif';
+import axios from 'axios';
+import Cookies from 'universal-cookie';
 
-export const Login = () => (
-  <Container className='mt-3'>
-    <h1 className='text-light letrica'>Inicio de sesion</h1>
-    <div className='pt-3'></div>
-    <div className='border-bottom border border-white mx-auto'></div>
-    <Row>
-      <Col>
-        <Form>
-          {/* email */}
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <div className='d-flex justify-content-start text-light letrica'>
-              <Form.Label>Email address</Form.Label>
-            </div>
-            <Form.Control type="email" placeholder="ingrese su email" />
-          </Form.Group>
+const baseUrl = "http://localhost:3000/usuarios";
+const cookies = new Cookies();
 
-          {/* contraseña */}
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <div className='d-flex justify-content-start text-light letrica'>
-              <Form.Label>Password</Form.Label>
-            </div>
-            <Form.Control type="password" placeholder="ingrese su contraseña" />
-          </Form.Group>
-          <Button variant="primary" type="submit" className='letrica'>
-            Submit
-          </Button>
-        </Form>
-      </Col>
-      <Col xs={10}>
-      <img src={logo} className="img-fluid" alt='lol'/>
-      </Col>
-    </Row>
+class Login extends Component {
+  state = {
+    form: {
+      Email: '',
+      Password: ''
+    }
+  }
 
-  </Container>
-)
+  handleChange = async e => {
+    await this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value
+      }
+    });
+  }
+
+  iniciarSesion = async () => {
+    await axios.get(baseUrl, { params: { Password: this.state.form.Email, Password: (this.state.form.Password) } })
+      .then(response => {
+        return response.data;
+      })
+      .then(response => {
+        if (response.length > 0) {
+          var respuesta = response[0];
+          cookies.set('id', respuesta.id, { path: "/" });
+          cookies.set('apellido_paterno', respuesta.apellido_paterno, { path: "/" });
+          cookies.set('apellido_materno', respuesta.apellido_materno, { path: "/" });
+          cookies.set('nombre', respuesta.nombre, { path: "/" });
+          cookies.set('username', respuesta.username, { path: "/" });
+          alert(`Bienvenido ${respuesta.nombre} ${respuesta.apellido_paterno}`);
+          window.location.href = "./menu";
+        } else {
+          alert('El usuario o la contraseña no son correctos');
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      })
+
+  }
+
+  componentDidMount() {
+    if (cookies.get('username')) {
+      window.location.href = "./menu";
+    }
+  }
+
+
+  render() {
+    return (
+      <Container className='mt-3'>
+          <div className="form-group">
+            <label>Usuario: </label>
+            <br />
+            <input
+              type="text"
+              className="form-control"
+              name="Email"
+              onChange={this.handleChange}
+            />
+            <br />
+            <label>Contraseña: </label>
+            <br />
+            <input
+              type="password"
+              className="form-control"
+              name="Password"
+              onChange={this.handleChange}
+            />
+            <br />
+            <button className="btn btn-primary" onClick={()=> this.iniciarSesion()}>Iniciar Sesión</button>
+          </div>
+    </Container>  
+    );
+  }
+}
+
+export default Login;
