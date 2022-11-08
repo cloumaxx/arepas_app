51bdec90ebc2e18e5353751c136a761343515122
Yulian <julianolomanolox@hotmail.com>
2022-11-07 20:09:27 -0500

    al loguear ahora los botones login y registro desaparecen, nesecario mejorar esta implementacion

--- arepas-app/src/components/NavBar.js
+++ arepas-app/src/components/NavBar.js
@@ -1,21 +1,58 @@
-import React from 'react';
-import {Navbar, Nav } from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
 import './NavBar.css';
 import logo from '../imagenes/arepa.png';
+import Cookies from 'universal-cookie';
 
-export const NavigationBar = () => (
-    <Navbar className='navbar border border-white ' >
-    <img src={logo} className="App-logo" alt='lol'/>
-    <Navbar.Brand className='text-light letrica'>Arepa-app</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-    <Navbar.Collapse id="basic-navbar-nav">        
-    <Nav className='me-auto' >
-    <Nav.Item><Nav.Link className='text-light letrica' href="/"  >Menu</Nav.Link></Nav.Item>
-    </Nav>
-      <Nav className="ms-auto">
-      <Nav.Item ><Nav.Link className='text-light letrica' href="/Login">Login</Nav.Link></Nav.Item>
-      <Nav.Item ><Nav.Link className='text-light letrica'href="./Register">Registrate</Nav.Link></Nav.Item>
-      </Nav>
-    </Navbar.Collapse>        
-  </Navbar >
-);
+const cookies = new Cookies();
+
+class NavigationBar extends Component {
+
+  componentDidMount() {
+    if (cookies.get('username')) {
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  render() {
+    if (!cookies.get('username')) {
+      return (
+        <Navbar className='navbar border border-white ' >
+          <img src={logo} className="App-logo" alt='lol' />
+          <Navbar.Brand className='text-light letrica'>Arepa-app</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className='me-auto' >
+              <Nav.Item><Nav.Link className='text-light letrica' href="/"  >Menu</Nav.Link></Nav.Item>
+            </Nav>
+            <Nav className="ms-auto">
+              <Nav.Item ><Nav.Link className='text-light letrica' href="/Login">Login</Nav.Link></Nav.Item>
+              <Nav.Item ><Nav.Link className='text-light letrica' href="./Register">Registrate</Nav.Link></Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar >
+      );
+    } if(cookies.get('username')) {
+      return(
+        <Navbar className='navbar border border-white ' >
+        <img src={logo} className="App-logo" alt='lol' />
+        <Navbar.Brand className='text-light letrica'>Arepa-app</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className='me-auto' >
+            <Nav.Item><Nav.Link className='text-light letrica' href="/"  >Menu</Nav.Link></Nav.Item>
+          </Nav>
+          <Nav className="ms-auto">
+
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar >
+      );
+    }
+
+  }
+}
+
+export default NavigationBar;
