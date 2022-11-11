902980bcbc41b58f8d28475f48ae2bb0094545fc
Yulian <julianolomanolox@hotmail.com>
2022-11-11 15:59:02 -0500

    paginacion y detalles falta terminar

--- arepas-app/src/App.js
+++ arepas-app/src/App.js
@@ -9,7 +9,7 @@ import { Layout } from './layout';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ThemeProvider from 'react-bootstrap/ThemeProvider'
 import {baseUrl} from './conexiones/urls'
-
+import {Details} from './paginas/Details'
 function App() {
   return (
     <ThemeProvider
@@ -24,6 +24,7 @@ function App() {
                 <Route exact path="/" element={<Menu baseUrl ={baseUrl} />} />
                 <Route path="/Login" element={<Login />} />
                 <Route path="/Register" element={<Register />} />
+                <Route path="/Details" element={<Details />} />
                 <Route path="*" component={NotFound} />
               </Routes>
             </Router>
