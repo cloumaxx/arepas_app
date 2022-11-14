6e96c5378dca0bd5b9f8e8d9ef2b50e54406e350
yulian david <julianolomanolox@hotmail.com>
2022-11-14 12:38:40 -0500

    api añadida y imagenes arregladas

---
+++ ShopApp/Shop.Api/Controllers/CustomersController.cs
@@ -0,0 +1,86 @@
+﻿using Application.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Api.Middleware;
+using Shop.Domain.Exceptions;
+using ShopApp.Application.Interfaces;
+using ShopApp.Domain.Entities;
+using System.Net;
+
+namespace ShopApp.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomersController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        // GET: api/<CustomersController>
+        [HttpGet]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(List<Customers>))]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError, Type = typeof(ErrorDetails))]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _customerService.GetAllAsync());
+        }
+
+        // GET api/<CustomersController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            try
+            {
+                return Ok(await _customerService.GetByIdAsync(id));
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        // POST api/<CustomersController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Customers category)
+        {
+            return Ok(await _customerService.AddAsync(category));
+        }
+
+        // PUT api/<CustomersController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] Customers customer)
+        {
+            try
+            {
+                return Ok(await _customerService.UpdateAsync(id, customer));
+            }
+            catch (BadRequestException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        // DELETE api/<CustomersController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await _customerService.RemoveAsync(id);
+                return Ok();
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+    }
+
+}
---
+++ ShopApp/Shop.Api/Extensions/ModuleCollectionExtensionProduct.cs
@@ -0,0 +1,26 @@
+﻿using ShopApp.Application.Services;
+using ShopApp.Application.Interfaces;
+using ShopApp.Domain.Interfaces.Repositories;
+using ShopApp.Infrastructure.Repositories;
+
+namespace Shop.Api.Extensions
+{
+    public static class ModuleCollectionExtensionProduct
+    {
+        public static IServiceCollection AddCoreModulesProduct(this IServiceCollection services)
+        {
+            // Services / Use Cases
+            //services.AddScoped<ShopApp.Application.Interfaces.IProductService,ProductService>();
+            services.AddScoped<IProductService, ProductService>();
+            return services;
+        }
+        public static IServiceCollection AddInfrastructureModulesProduct(this IServiceCollection services)
+        {
+            // Repositories
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+
+            return services;
+        }
+    }
+}
