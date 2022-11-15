87ff6609934a6ee6bcbf7f7cdb2fc48d7c4887c8
yulian david <julianolomanolox@hotmail.com>
2022-11-15 15:18:19 -0500

    preimplementacion

--- arepas-app/src/components/Login.js
+++ arepas-app/src/components/Login.js
@@ -40,7 +40,7 @@ class Login extends Component {
           cookies.set('nombre', respuesta.nombre, { path: "/" });
           cookies.set('username', respuesta.username, { path: "/" });
           alert(`Bienvenido ${respuesta.nombre} ${respuesta.username}`);
-          window.location.href = "/";
+          window.location.href = "./menu";
         } else {
           alert('El usuario o la contraseña no son correctos');
         }
@@ -53,7 +53,7 @@ class Login extends Component {
 
   componentDidMount() {
     if (cookies.get('username')) {
-      window.location.href = "/";
+      window.location.href = "./menu";
     }
   }
 
@@ -61,32 +61,40 @@ class Login extends Component {
   render() {
     return (
       <Container className='mt-3'>
-        <h1 className='text-light letrica'>Inicio de sesion</h1>
+        <h1 className='text-light letrica'>Inicio de sesión</h1>
         <div className='pt-3'></div>
         <div className='border-bottom border border-white mx-auto'></div>
 
-        <Form.Group className="form-group mb-3" controlId="formBasicEmail">
-          <div className='d-flex justify-content-start text-light letrica'>
-            <Form.Label>Email address</Form.Label>
+
+        <Form.Group className="form-group mt-3" controlId="formBasicEmail">
+
+          <div className='justify-content-start text-light letrica'>
+          <h4 className="center">Correo electronico</h4>
           </div>
+     
           <input
             type="text"
-            className="form-control"
+            className="form-control-lg "
             name="Email"
             onChange={this.handleChange}
-            placeholder="ingrese su email"
+            placeholder="ingrese su correo"
+            size={25}
           />
-          <div className='d-flex justify-content-start text-light letrica'>
-              <Form.Label>Password</Form.Label>
-            </div>
+        
+          <p></p>
+  <div className='justify-content-start text-light letrica'>
+          <h4 className="center">Contraseña</h4>
+          </div>
           <input
             type="password"
-            className="form-control"
+            className="form-control-lg"
             name="Password"
             onChange={this.handleChange}
             placeholder="ingrese su contraseña"
+            size={25}
           />
           <br />
+          <p></p>
           <button className="btn btn-primary" onClick={() => this.iniciarSesion()}>Iniciar Sesión</button>
         </Form.Group>
         <img src={logo} className="img-fluid" alt='lol'/>
--- arepas-app/src/components/Register.js
+++ arepas-app/src/components/Register.js
@@ -41,60 +41,63 @@ export const Register = () => {
   }
 
   return (
-    <Container className='mt-3'>
-      <h1 className='text-light letrica'>Registro</h1>
-      <div className='pt-3'></div>
-      <div className='border-bottom border border-white mx-auto'></div>
-      <Form.Group className="form-group mb-3" controlId="formBasicEmail">
+    <Container className='mt-9'>
+    <h1 className='text-light letrica'>Registro</h1>
+    <div className='pt-3'></div>
+    <div className='border-bottom border border-white mx-auto'></div>
+    <Form.Group className="form-group mb-3" controlId="formBasicEmail">
+    <p></p>
+      <div class="one-half last">
+      <div className='justify-content-start text-light letrica'>
+      <h4 className="center">Correo electronico</h4>
+      </div>
+      <input placeholder='su correo' type="Email"  class="form-control-lg" id="exampleFormControlInput1" value={fromData.Email} onChange={(e) => setFormData({ ...fromData, Email: e.target.value })} size={25} />
+      <p></p>
+      <div className='justify-content-start text-light letrica'>
+      <h4 className="center">Contraseña </h4>
+      </div>
+      <input placeholder='contraseña' type="Password" class="form-control-lg" id="exampleFormControlInput1" value={fromData.Password} onChange={(e) => setFormData({ ...fromData, Password: e.target.value })} size={25} />
+      <p></p>
+      <div className='justify-content-start text-light letrica'>
+      <h4 className="center">Nombre</h4>
+      </div>
+      <input placeholder='Su Nombre' type="name" class="form-control-lg" id="exampleFormControlInput1" value={fromData.FirstName} onChange={(e) => setFormData({ ...fromData, FirstName: e.target.value })} size={25} />
+      <p></p>
+      <div className='justify-content-start text-light letrica'>
+      <h4 className="center">Apellido</h4>
+      </div>
+      <input placeholder='Su Apellido' type="name" class="form-control-lg" id="exampleFormControlInput1" value={fromData.LastName} onChange={(e) => setFormData({ ...fromData, LastName: e.target.value })} size={25} />
+      </div>
+      <p></p>
+      <div class="one-half">
+      <div className='justify-content-start text-light letrica'>
+      <h4 className="center">Fecha de Nacimiento</h4>
+      </div>
+      <input placeholder='Su fecha de nacimiento' type="date" class="form-control-lg" id="exampleFormControlInput1" value={fromData.BirthofDate} onChange={(e) => setFormData({ ...fromData, BirthofDate: e.target.value })} />
 
-        <div className='d-flex justify-content-start text-light letrica'>
-          <Form.Label>Correo Electronico</Form.Label>
-        </div>
-        <input placeholder='su correo' type="Email" class="form-control" id="exampleFormControlInput1" value={fromData.Email} onChange={(e) => setFormData({ ...fromData, Email: e.target.value })} />
+      <p></p>
+      <div className='justify-content-start text-light letrica'>
+      <h4 className="center">Fecha de Registro</h4>
+      </div>
+      <input placeholder='Su fecha de Registro' type="date" class="form-control-lg" id="exampleFormControlInput1" value={fromData.RegisterDate} onChange={(e) => setFormData({ ...fromData, RegisterDate: e.target.value })} />
 
-        <div className='d-flex justify-content-start text-light letrica'>
-          <Form.Label>contraseña</Form.Label>
-        </div>
-        <input placeholder='contraseña' type="Password" class="form-control" id="exampleFormControlInput1" value={fromData.Password} onChange={(e) => setFormData({ ...fromData, Password: e.target.value })} />
-
-        <div className='d-flex justify-content-start text-light letrica'>
-          <Form.Label>Nombre</Form.Label>
-        </div>
-        <input placeholder='Su Nombre' type="name" class="form-control" id="exampleFormControlInput1" value={fromData.FirstName} onChange={(e) => setFormData({ ...fromData, FirstName: e.target.value })} />
-
-        <div className='d-flex justify-content-start text-light letrica'>
-          <Form.Label>Apellido</Form.Label>
-        </div>
-        <input placeholder='Su Apellido' type="name" class="form-control" id="exampleFormControlInput1" value={fromData.LastName} onChange={(e) => setFormData({ ...fromData, LastName: e.target.value })} />
-
-        <div className='d-flex justify-content-start text-light letrica'>
-          <Form.Label>Fecha de Nacimiento</Form.Label>
-        </div>
-        <input placeholder='Su fecha de nacimiento' type="date" class="form-control" id="exampleFormControlInput1" value={fromData.BirthofDate} onChange={(e) => setFormData({ ...fromData, BirthofDate: e.target.value })} />
-
-
-        <div className='d-flex justify-content-start text-light letrica'>
-          <Form.Label>Fecha de Registro</Form.Label>
-        </div>
-        <input placeholder='Su fecha de Registro' type="date" class="form-control" id="exampleFormControlInput1" value={fromData.RegisterDate} onChange={(e) => setFormData({ ...fromData, RegisterDate: e.target.value })} />
-
-        
-        <div className='d-flex justify-content-start text-light letrica'>
-          <Form.Label>Direcion</Form.Label>
-        </div>
-        <input placeholder='Su direccion' type="Address" class="form-control" id="exampleFormControlInput1" value={fromData.Address} onChange={(e) => setFormData({ ...fromData, Address: e.target.value })} />
-
-        <div className='d-flex justify-content-start text-light letrica'>
-          <Form.Label>Telefono</Form.Label>
-        </div>
-        <input placeholder='Su telefono' type="phone" class="form-control" id="exampleFormControlInput1" value={fromData.PhoneNumber} onChange={(e) => setFormData({ ...fromData, PhoneNumber: e.target.value })} />
-
-
-        <div class="mb-3">
-          <button className='btn btn-success' onClick={handleFormSubmit}>Registrate</button>
-        </div>
-        <img src={logo} className="img-fluid" alt='lol' />
-      </Form.Group>
-    </Container>
+      <p></p>      
+      <div className='justify-content-start text-light letrica'>
+      <h4 className="center">Dirección</h4>
+      </div>
+      <input placeholder='Su dirección' type="Address" class="form-control-lg" id="exampleFormControlInput1" value={fromData.Address} onChange={(e) => setFormData({ ...fromData, Address: e.target.value })} size={25} />
+      <p></p> 
+      <div className='justify-content-start text-light letrica'>
+      <h4 className="center">Telefono</h4>
+      </div>
+      <input placeholder='Su telefono' type="phone" class="form-control-lg" id="exampleFormControlInput1" value={fromData.PhoneNumber} onChange={(e) => setFormData({ ...fromData, PhoneNumber: e.target.value })} size={25} />
+      <p></p>
+      </div>
+      <div class="mb-3">
+        <button className='btn btn-success' onClick={handleFormSubmit}>Registrate</button>
+      </div>
+      <img src={logo} className="img-fluid" alt='lol' width="260" height="220" />
+    </Form.Group>
+  </Container>
   )
 }
