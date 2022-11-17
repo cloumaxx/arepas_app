9578976bef7aa5283384af384891c590c33488bf
Cristian Giraldo <cristian_gira@hotmail.com>
2022-11-17 16:22:57 -0500

    Arreglo?

---
+++ ShopApp/Shop.Domain/Interfaces/Repositories/ICustomerRepository.cs
@@ -0,0 +1,9 @@
+﻿using ShopApp.Domain.Common;
+using ShopApp.Domain.Entities;
+
+namespace ShopApp.Domain.Interfaces.Repositories
+{
+    public interface ICustomerRepository : IRepository<Customers>
+    {
+    }
+}
--- arepas-app/src/paginas/Register.js
+++ arepas-app/src/paginas/Register.js
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import DatePicker from "react-multi-date-picker"
+import {  Container } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import logo from '../imagenes/arepagif.gif';
+import './Menu.css'
 
 export const Register = () => {
 
   const [fromData, setFormData] = useState({
-    nombre: "",
-    apellido: "",
-    username: "",
-    password: "",
+    Email: "",
+    Password: "",
+    FirstName: "",
+    LastName: "",
+    BirthofDate: "",
+    RegisterDate: "",
+    Address: "",
+    PhoneNumber: ""
   });
 
   const handleFormSubmit = async (e) => {
@@ -22,42 +29,75 @@ export const Register = () => {
     }
 
     setFormData({
-      nombre: "",
-      apellido: "",
-      username: "",
-      password: "",
+      Email: "",
+      Password: "",
+      FirstName: "",
+      LastName: "",
+      BirthofDate: "",
+      RegisterDate: "",
+      Address: "",
+      PhoneNumber: ""
     });
   }
 
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-7">
-          <h1 className="row"> Register </h1>
-        </div>
-        <div class="mb-3">
-          <label for="exampleFormControlInput1" class="form-label">nombre</label>
-          <input type="nombre" class="form-control" id="exampleFormControlInput1" value={fromData.nombre} onChange={(e) => setFormData({ ...fromData, nombre: e.target.value })} />
-        </div>
-        <div class="mb-3">
-          <label for="exampleFormControlInput1" class="form-label">apellido</label>
-          <input type="apellido" class="form-control" id="exampleFormControlInput1" value={fromData.apellido} onChange={(e) => setFormData({ ...fromData, apellido: e.target.value })} />
-        </div>
-        <div class="mb-3">
-          <label for="exampleFormControlInput1" class="form-label">Email</label>
-          <input type="username" class="form-control" id="exampleFormControlInput1" value={fromData.username} onChange={(e) => setFormData({ ...fromData, username: e.target.value })} />
-        </div>
-        <div class="mb-3">
-          <label for="exampleFormControlInput1" class="form-label">password</label>
-          <input type="password" class="form-control" id="exampleFormControlInput1" value={fromData.password} onChange={(e) => setFormData({ ...fromData, password: e.target.value })} />
-        </div>
-        <div class="mb-3">
-          <DatePicker className="bg-dark " />
-        </div>
-        <div class="mb-3">
-          <button className='btn btn-success' onClick={handleFormSubmit}>Registrate</button>
-        </div>
+    <Container className='mt-9'>
+    <h1 className='text-light letrica'>Registro</h1>
+    <div className='pt-3'></div>
+    <div className='border-bottom border border-white mx-auto'></div>
+    <Form.Group className="form-group mb-3" controlId="formBasicEmail">
+    <p></p>
+      <div class="one-half last">
+      <div className='justify-content-start text-light letrica'>
+      <h4 className="center">Correo electronico</h4>
       </div>
-    </div>
+      <input placeholder='su correo' type="Email"  class="form-control-lg" id="exampleFormControlInput1" value={fromData.Email} onChange={(e) => setFormData({ ...fromData, Email: e.target.value })} size={25} />
+      <p></p>
+      <div className='justify-content-start text-light letrica'>
+      <h4 className="center">Contraseña </h4>
+      </div>
+      <input placeholder='contraseña' type="Password" class="form-control-lg" id="exampleFormControlInput1" value={fromData.Password} onChange={(e) => setFormData({ ...fromData, Password: e.target.value })} size={25} />
+      <p></p>
+      <div className='justify-content-start text-light letrica'>
+      <h4 className="center">Nombre</h4>
+      </div>
+      <input placeholder='Su Nombre' type="name" class="form-control-lg" id="exampleFormControlInput1" value={fromData.FirstName} onChange={(e) => setFormData({ ...fromData, FirstName: e.target.value })} size={25} />
+      <p></p>
+      <div className='justify-content-start text-light letrica'>
+      <h4 className="center">Apellido</h4>
+      </div>
+      <input placeholder='Su Apellido' type="name" class="form-control-lg" id="exampleFormControlInput1" value={fromData.LastName} onChange={(e) => setFormData({ ...fromData, LastName: e.target.value })} size={25} />
+      </div>
+      <p></p>
+      <div class="one-half">
+      <div className='justify-content-start text-light letrica'>
+      <h4 className="center">Fecha de Nacimiento</h4>
+      </div>
+      <input placeholder='Su fecha de nacimiento' type="date" class="form-control-lg" id="exampleFormControlInput1" value={fromData.BirthofDate} onChange={(e) => setFormData({ ...fromData, BirthofDate: e.target.value })} />
+
+      <p></p>
+      <div className='justify-content-start text-light letrica'>
+      <h4 className="center">Fecha de Registro</h4>
+      </div>
+      <input placeholder='Su fecha de Registro' type="date" class="form-control-lg" id="exampleFormControlInput1" value={fromData.RegisterDate} onChange={(e) => setFormData({ ...fromData, RegisterDate: e.target.value })} />
+
+      <p></p>      
+      <div className='justify-content-start text-light letrica'>
+      <h4 className="center">Dirección</h4>
+      </div>
+      <input placeholder='Su dirección' type="Address" class="form-control-lg" id="exampleFormControlInput1" value={fromData.Address} onChange={(e) => setFormData({ ...fromData, Address: e.target.value })} size={25} />
+      <p></p> 
+      <div className='justify-content-start text-light letrica'>
+      <h4 className="center">Telefono</h4>
+      </div>
+      <input placeholder='Su telefono' type="phone" class="form-control-lg" id="exampleFormControlInput1" value={fromData.PhoneNumber} onChange={(e) => setFormData({ ...fromData, PhoneNumber: e.target.value })} size={25} />
+      <p></p>
+      </div>
+      <div class="mb-3">
+        <button className='btn btn-success' onClick={handleFormSubmit}>Registrate</button>
+      </div>
+      <img src={logo} className="img-fluid" alt='lol' width="260" height="220" />
+    </Form.Group>
+  </Container>
   )
 }
