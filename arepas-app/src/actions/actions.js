9ed415a7b187797e92f7e4278248a246c0115546
Cristian Giraldo <cristian_gira@hotmail.com>
2022-11-20 16:45:01 -0500

    Últimos cambios

---
+++ arepas-app/src/actions/actions.js
@@ -0,0 +1,20 @@
+// log actions
+export const log = () =>{
+    return{
+        type: 'log'
+    }
+}
+
+export const logout = () =>{
+    return{
+        type: 'logout'
+    }
+}
+
+// shopping car actions
+export const shopitem = (arepa) =>{
+    return{
+        type: 'shop',
+        payload:arepa
+    }
+}
