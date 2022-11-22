3cdf8ee83483e43d7ec6b28d254ad6628c763db9
Cristian Giraldo <cristian_gira@hotmail.com>
2022-11-21 21:59:18 -0500

    xD

--- arepas-app/src/conexiones/urls.js
+++
@@ -1,3 +0,0 @@
-
-export const baseUrl = "http://localhost:3000/arepas?_page=1&_limit=3";
-export const imagesUrl = "https://raw.githubusercontent.com/Jucer74/WebDevelopment/main/Proyecto/ImageProducts/";
