6a3af8d6858f1c5bf958bebb9528685a54bebdd2
Cristian Giraldo <cristian_gira@hotmail.com>
2022-11-21 22:00:55 -0500

    Ultima version

---
+++ arepaApi/Shop.Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShopApp.Domain.Interfaces.Repositories;
+using ShopApp.Domain.Entities;
+using ShopApp.Infrastructure.Common;
+using ShopApp.Infrastructure.Context;
+
+namespace ShopApp.Infrastructure.Repositories
+{
+    public class CustomerRepository : Repository<Customers>, ICustomerRepository
+    {
+        public CustomerRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+        }
+    }
+}
