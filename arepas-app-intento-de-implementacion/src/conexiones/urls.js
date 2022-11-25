24d09eaa5c31f239365bdd69628261b0076dae26
marcook888 <marcosrojas772@gmail.com>
2022-11-24 21:55:50 -0500

    se intento

---
+++ arepas-app-intento-de-implementacion/src/conexiones/urls.js
@@ -0,0 +1,4 @@
+
+export const baseUrl = "https://localhost:7164/api/v1/Products?Page=1&Limit=3";
+export const Orders = "http://localhost:3000/Orders";
+export const imagesUrl = "https://raw.githubusercontent.com/Jucer74/WebDevelopment/main/Proyecto/ImageProducts/";
